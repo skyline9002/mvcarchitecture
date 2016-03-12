36246a58d00cb4cccbf315d166be4da8c56ca748
skyline9002 <simonhuang211@gmail.com>
2016-03-13 01:09:47 +1300

    Split to multiple partial views in cv

--- Store.Web/Controllers/CVController.cs
+++ Store.Web/Controllers/CVController.cs
@@ -20,5 +20,65 @@ namespace Store.Web.Controllers
             return PartialView("_IntroductionSection");
         }
 
+        [ChildActionOnly]
+        public ActionResult ContactSection()
+        {
+            return PartialView("_ContactSection");
+        }
+
+        [ChildActionOnly]
+        public ActionResult ProfileSection()
+        {
+            return PartialView("_ProfileSection");
+        }
+        
+        [ChildActionOnly]
+        public ActionResult StatsSection()
+        {
+            return PartialView("_StatsSection");
+        }
+
+        [ChildActionOnly]
+        public ActionResult CalloutSection()
+        {
+            return PartialView("_CalloutSection");
+        }
+
+        [ChildActionOnly]
+        public ActionResult ClientSection()
+        {
+            return PartialView("_ClientSection");
+        }
+
+        [ChildActionOnly]
+        public ActionResult ResumeSection()
+        {
+            return PartialView("_ResumeSection");
+        }
+
+        [ChildActionOnly]
+        public ActionResult PortfolioSection()
+        {
+            return PartialView("_PortfolioSection");
+        }
+
+        [ChildActionOnly]
+        public ActionResult FooterContactSection()
+        {
+            return PartialView("_FooterContactSection");
+        }
+
+        [ChildActionOnly]
+        public ActionResult FooterSection()
+        {
+            return PartialView("_FooterSection");
+        }
+
+        [ChildActionOnly]
+        public ActionResult ServiceSection()
+        {
+            return PartialView("_ServiceSection");
+        }
+
     }
 }
